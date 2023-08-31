0dee1cb1a1644b4fe303f22b616b74f287dae6fd
jjperezaguinaga <me@jjperezaguinaga.com>
2023-08-31 07:34:03 +0000

    feature: All steps included

--- app/src/components/MPCWallet/MPCWallet.tsx
+++ app/src/components/MPCWallet/MPCWallet.tsx
@@ -1,21 +1,42 @@
-import { Button, Code, Flex, Heading, useToast, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Code,
+  Flex,
+  Heading,
+  useToast,
+  Text,
+  Box,
+  IconButton,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 const { getHash } = require("emoji-hash-gen");
-import { DKGP1, DKGP2 } from "@safeheron/two-party-mpc-adapter";
+import {
+  DKGP1,
+  DKGP2,
+  JsonObject,
+  deriveAddressFromCurvePoint,
+} from "@safeheron/two-party-mpc-adapter";
 
-import { abbreviate } from "../../helpers";
 import { QRScanner } from "../QRScanner";
 import { QRCodeImage } from "../QRCodeImage";
 import { atomWithStorage } from "jotai/utils";
 import { useAtom } from "jotai";
 import { MPCValue } from "./MPCValue";
 import { MPCButton } from "./MPCButton";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
-type STEP = "step_0" | "step_1_1" | "step_1_2" | "step_2_1" | "step_2_2";
+type STEP =
+  | "step_0"
+  | "step_1_1"
+  | "step_1_2"
+  | "step_1_3"
+  | "step_2_1"
+  | "step_2_2";
 
 const messageOneAtom = atomWithStorage("messageOne", "");
 const messageTwoAtom = atomWithStorage("messageTwo", "");
 const messageThreeAtom = atomWithStorage("messageThree", "");
+const messageDoneForTwoAtom = atomWithStorage("messageDoneForTwo", "");
 
 export const MPCWallet = ({
   dkg,
@@ -35,6 +56,12 @@ export const MPCWallet = ({
   const [messageOne, setMessageOne] = useAtom(messageOneAtom);
   const [messageTwo, setMessageTwo] = useAtom(messageTwoAtom);
   const [messageThree, setMessageThree] = useAtom(messageThreeAtom);
+  const [messageDoneForTwo, setMessageDoneForTwo] = useAtom(
+    messageDoneForTwoAtom
+  );
+  const [keyshare, setKeyshare] = useState<
+    JsonObject.JsonObject_KeyShare2 | JsonObject.JsonObject_KeyShare1
+  >();
 
   const stepOne = async (pub: string) => {
     console.log("(🔑,1️⃣) 🟠 Executing step one, you should be wallet-1");
@@ -52,9 +79,35 @@ export const MPCWallet = ({
     return message2;
   };
 
+  const stepThree = async (messageTwo: string) => {
+    console.log("(🔑,3️⃣) 🟠 Executing step three, you should be wallet-1");
+    const dkgp1 = dkg as DKGP1;
+    const message3 = await dkgp1.step2(messageTwo); // This might fail if we have lost the context
+    console.log("(🔑,3️⃣) 🟢 Step three executed [message]", message3);
+    return message3;
+  };
+
+  const stepFour = async (messageThree: string) => {
+    console.log("(🔑,4️⃣) 🟠 Executing step four, you should be wallet-2");
+    const dkgp2 = dkg as DKGP2;
+    const doneMessage = await dkgp2.step2(messageTwo); // This might fail if we have lost the context
+    console.log("(🔑,4️⃣) 🟢 Step four executed [message]", doneMessage);
+    const keyshare = dkgp2.exportKeyShare2();
+    return { message: doneMessage, keyshare };
+  };
+
+  const stepFive = async (doneMessageForTwo: string) => {
+    console.log("(🔑,5️⃣) 🟠 Executing step five, you should be wallet-1");
+    const dkgp1 = dkg as DKGP1;
+    const keyshare = await dkgp1.step3(doneMessageForTwo); // This might fail if we have lost the context
+    console.log("(🔑,5️⃣) 🟢 Step five executed, no more messages");
+    return { keyshare };
+  };
+
   const STEP_ONE_ONE_COMPLETED = messageOne.length > 0;
   const STEP_TWO_ONE_COMPLETED = messageTwo.length > 0;
   const STEP_ONE_TWO_COMPLETED = messageThree.length > 0;
+  const STEP_TWO_TWO_COMPLETED = messageDoneForTwo.length > 0;
 
   useEffect(() => {
     const loadPayload = async () => {
@@ -65,11 +118,26 @@ export const MPCWallet = ({
         setMessageOne(message);
         setCurrentPayload(newPayload);
       }
+      if (currentStep == "step_1_2") {
+        const message = await stepThree(qrPayload);
+        setMessageThree(message);
+        setCurrentPayload(message);
+      }
+      if (currentStep == "step_1_3") {
+        const { keyshare } = await stepFive(qrPayload);
+        setKeyshare(keyshare);
+      }
       if (currentStep == "step_2_1") {
         const [_messageOne, pub] = qrPayload.split(",");
         const message = await stepTwo(_messageOne, pub);
         setMessageTwo(message);
         setCurrentPayload(message);
+      }
+      if (currentStep == "step_2_2") {
+        const { message, keyshare } = await stepFour(qrPayload);
+        setMessageDoneForTwo(message);
+        setCurrentPayload(message);
+        setKeyshare(keyshare);
       }
     };
     qrPayload && loadPayload();
@@ -82,37 +150,72 @@ export const MPCWallet = ({
   useEffect(() => {
     setMessageOne("");
     setMessageTwo("");
+    setMessageThree("");
+    setMessageDoneForTwo("");
   }, [priv]);
 
   return (
     <Flex mt="2" textAlign={"center"} gap="10" flexDir={"column"}>
+      <Box>
+        <Text fontSize={'sm'}>Developer Tools</Text>
+        <IconButton
+          w="fit-content"
+          mx="auto"
+          icon={!enableQRScanner ? <ViewIcon /> : <ViewOffIcon />}
+          onClick={() => setEnableQRScanner(!enableQRScanner)}
+          aria-label="Toggle camera"
+        />
+      </Box>
       <Flex flexDir={"column"} gap="2">
         {pub && <MPCValue label={"Pub Address"} value={pub} />}
         {messageOne && <MPCValue label="Message one" value={messageOne} />}
         {messageTwo && <MPCValue label="Message two" value={messageTwo} />}
+        {messageThree && (
+          <MPCValue label="Message three" value={messageThree} />
+        )}
+        {messageDoneForTwo && (
+          <MPCValue label="Done message" value={messageDoneForTwo} />
+        )}
+        {keyshare && (
+          <MPCValue
+            label="Keyshare"
+            value={deriveAddressFromCurvePoint(keyshare.Q.x, keyshare.Q.y)}
+          />
+        )}
         {currentPayload && <QRCodeImage payload={currentPayload} />}
         <Text letterSpacing={"10px"}>{getHash(currentPayload)}</Text>
         <Heading fontSize={"xl"} as="h3">
           Scanner
         </Heading>
-        {instance == 1 && currentStep == "step_0" && (
+        {instance == 1 && (
           <MPCButton
             hasBeenCompleted={STEP_ONE_ONE_COMPLETED}
             onCompletionMessage="✅ Scanned pub key of Wallet 2"
-            defaultMessage="📸 Scan pubKey to start DKG handshake"
+            defaultMessage="1️⃣ Scan pubKey to start DKG handshake"
             onClickHandler={() => {
               setCurrentStep("step_1_1");
               setEnableQRScanner(!enableQRScanner);
             }}
           />
         )}
-        {instance == 1 && currentStep == "step_1_1" && (
+        {instance == 1 && STEP_ONE_ONE_COMPLETED && (
           <MPCButton
             hasBeenCompleted={STEP_ONE_TWO_COMPLETED}
             onCompletionMessage="✅ Scanned message from Wallet 2"
-            defaultMessage="📸 Scan pubKey to start DKG handshake"
+            defaultMessage="3️⃣ Scan signed message 2 to complete DKG"
             onClickHandler={() => {
               setCurrentStep("step_1_2");
+              setEnableQRScanner(!enableQRScanner);
+            }}
+          />
+        )}
+        {instance == 1 && STEP_ONE_TWO_COMPLETED && (
+          <MPCButton
+            hasBeenCompleted={!!keyshare}
+            onCompletionMessage="✅ Scanned message and completed setup"
+            defaultMessage="5️⃣ Scan done message from 2 to get key"
+            onClickHandler={() => {
+              setCurrentStep("step_1_3");
               setEnableQRScanner(!enableQRScanner);
             }}
           />
@@ -121,9 +224,20 @@ export const MPCWallet = ({
           <MPCButton
             hasBeenCompleted={STEP_TWO_ONE_COMPLETED}
             onCompletionMessage="✅ Scanned message one from Wallet 1"
-            defaultMessage="📸 Scan message one to agree DKG handshake"
+            defaultMessage="2️⃣ Scan message one to agree DKG handshake"
             onClickHandler={() => {
               setCurrentStep("step_2_1");
+              setEnableQRScanner(!enableQRScanner);
+            }}
+          />
+        )}
+        {instance == 2 && STEP_TWO_ONE_COMPLETED && (
+          <MPCButton
+            hasBeenCompleted={STEP_TWO_TWO_COMPLETED}
+            onCompletionMessage="✅ Scanned all messages needed from Wallet 1"
+            defaultMessage="4️⃣ Scan last message to derive keyshare"
+            onClickHandler={() => {
+              setCurrentStep("step_2_2");
               setEnableQRScanner(!enableQRScanner);
             }}
           />
