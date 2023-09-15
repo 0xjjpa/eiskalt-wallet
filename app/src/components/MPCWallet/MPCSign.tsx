3fd041a973f44c4c6aa3243ac81b14e92a7cdaae
jjperezaguinaga <me@jjperezaguinaga.com>
2023-09-15 14:48:29 +0000

    feature: ping-pong for step_0 ready

--- app/src/components/MPCWallet/MPCSign.tsx
+++ app/src/components/MPCWallet/MPCSign.tsx
@@ -1,15 +1,14 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import { JsonObject, Encryptor } from "@safeheron/two-party-mpc-adapter";
-import { PubCurveBasePoint } from "@safeheron/two-party-mpc-adapter/dist/enc/Encryptor"
-import type BN from 'bn.js'
-import { curve } from 'elliptic';
+import { PubCurveBasePoint } from "@safeheron/two-party-mpc-adapter/dist/enc/Encryptor";
+import type BN from "bn.js";
+import { curve } from "elliptic";
 
 import { mpcSDK } from "../../lib/mpc";
 import { Supabase } from "../../lib/supabase";
 import { STEP } from "./MPCWallet";
 import { useEffect, useState } from "react";
 import { cosignerHandler } from "../../lib/pusher";
-
 
 export const MPCSign = ({
   sessionId,
@@ -22,35 +21,36 @@ export const MPCSign = ({
   channel: Supabase;
   keyshare: JsonObject.JsonObject_KeyShare2 | JsonObject.JsonObject_KeyShare1;
 }) => {
-
   const [privKey, setPrivKey] = useState<BN>();
   const [pubKey, setPubKey] = useState<PubCurveBasePoint>();
 
   useEffect(() => {
-    console.log(`(🔌,ℹ️ ) Listening to client on instance ${instance}`)
-    privKey && channel.listen("sign", cosignerHandler({ instance, privKey, keyshare }))
-  }, [privKey])
-  
+    console.log(`(🔌,ℹ️ ) Listening to client on instance ${instance}`);
+    privKey &&
+      channel.listen("sign", cosignerHandler({ instance, privKey, keyshare, pubKey, channel }));
+  }, [privKey]);
+
   useEffect(() => {
     const generateKeys = async () => {
-      const {priv, pub} = await Encryptor.generateKeyPaire()
+      const { priv, pub } = await Encryptor.generateKeyPaire();
       setPrivKey(priv);
       setPubKey(pub);
-      
-      console.log('(🔑,ℹ️) PubKey generated', pub)
-    }
-    generateKeys()
+
+      console.log("(🔑,ℹ️) PubKey generated", pub);
+    };
+    generateKeys();
     return () => {
       setPrivKey(null);
       setPubKey(null);
-    }
-  }, [])
+    };
+  }, []);
+
   const generatePayload = (step: STEP) => {
     switch (step) {
       case "step_0":
         return JSON.stringify({
-          x: pubKey.getX().toString('hex'),
-          y: pubKey.getY().toString('hex'),
+          x: pubKey.getX().toString("hex"),
+          y: pubKey.getY().toString("hex"),
         });
       default:
         console.log(`(📦,ℹ️) Step ${step} has no been defined.`);
@@ -59,7 +59,7 @@ export const MPCSign = ({
 
   const handleCosignature = async () => {
     console.log("(🖊️,ℹ️) Starting co-signature...");
-    const payload = generatePayload("step_0")
+    const payload = generatePayload("step_0");
     console.log("First step", payload);
     mpcSDK({
       id: sessionId,
@@ -71,7 +71,9 @@ export const MPCSign = ({
       channel,
     });
   };
-  return (
+  return instance == 2 ? (
     <Button onClick={handleCosignature}>🖊️ Co-sign demo transaction</Button>
+  ) : (
+    <Text fontSize={'xs'}>Start the signature from 📱</Text>
   );
 };
--- app/src/lib/pusher.ts
+++ app/src/lib/pusher.ts
@@ -3,6 +3,8 @@ import BN from 'bn.js'
 import { JsonObject, Encryptor, SignerP1, SignerP2 } from "@safeheron/two-party-mpc-adapter";
 import { PubCurveBasePoint } from "@safeheron/two-party-mpc-adapter/dist/enc/Encryptor"
 import { ec as EC } from 'elliptic';
+import { mpcSDK } from "./mpc";
+import { Supabase } from "./supabase";
 
 
 export type WebsocketPayload = {
@@ -17,45 +19,68 @@ type SupabaseWebsocketPayload = {
   payload: WebsocketPayload
 }
 
-const txObject = {
-  nonce: 0,
-  to: "0x6442c72aBD1a9d14c303277a8C994Fae295b6BCB",
-  value: 0,
-  chainId: 5,
-  data: '',
-  maxFeePerGas: '100',
-  maxPriorityFeePerGas: '1000',
-  gasLimit: 21000
-}
 
-
-export function cosignerHandler({ instance, keyshare, privKey }: { instance: 1 | 2, keyshare: JsonObject.JsonObject_KeyShare2 | JsonObject.JsonObject_KeyShare1, privKey: BN }) {
-  let counter = 0
+export function cosignerHandler({ instance, keyshare, privKey, pubKey, channel }: { instance: 1 | 2, keyshare: JsonObject.JsonObject_KeyShare2 | JsonObject.JsonObject_KeyShare1, privKey: BN, pubKey: PubCurveBasePoint, channel: Supabase }) {
+  let stepCounter = 0
   let signerP1, signerP2
+  const txObject = {
+    nonce: 0,
+    to: "0x6442c72aBD1a9d14c303277a8C994Fae295b6BCB",
+    value: 0,
+    chainId: 5,
+    data: '',
+    maxFeePerGas: '100',
+    maxPriorityFeePerGas: '1000',
+    gasLimit: 21000
+  }
   return async function cosignerListener(data: SupabaseWebsocketPayload) {
-    console.log("Listener Data", data);
     const { payload } = data;
     if (payload.instance != instance) {
       const { step, payload: socketPayload } = payload;
-      console.log('(🪜,ℹ️) Step obtained', step);
+      console.log(`(🪜,ℹ️) Step "${step}" obtained by device #${instance}, sent by ${payload.instance}`);
       switch (step) {
         case 'step_0': {
-          console.log('(0️⃣,ℹ️) Step 0 to be executed');
           const ec = new EC('secp256k1');
-          const parsed = JSON.parse(socketPayload);
-          const pubKey: PubCurveBasePoint = ec.curve.point(parsed.x, parsed.y);
-          console.log('(🔑,ℹ️) PubKey parsed', pubKey, privKey, keyshare)
           if (instance == 1) {
-            signerP1 = new SignerP1(keyshare, Encryptor.encodeAuthPriv(privKey), Encryptor.encodeAuthPub(pubKey))
-            console.log('(💻,ℹ️) Signer for device 1 created');
+            console.log('(0️⃣-💻,ℹ️) Step 0 to be executed in 💻');
+            const parsed = JSON.parse(socketPayload);
+            const otherDevicePubKey: PubCurveBasePoint = ec.curve.point(parsed.x, parsed.y);
+            console.log('(🔑,ℹ️) PubKey parsed', pubKey, privKey, keyshare)
+            signerP1 = new SignerP1(keyshare, Encryptor.encodeAuthPriv(privKey), Encryptor.encodeAuthPub(otherDevicePubKey))
+            await signerP1.createContext(txObject)
+            const message1 = await signerP1.step1()
+            console.log('(💻,ℹ️) Signer for device 1 created, and signed message1', message1);
+
+            // Ping for step_0
+            mpcSDK({
+              id: payload.id,
+              step: "step_0",
+              payload: JSON.stringify({
+                x: pubKey.getX().toString("hex"),
+                y: pubKey.getY().toString("hex"),
+                message1
+              }),
+              instance,
+              endpoint: "sign",
+              client: "supabase",
+              channel,
+            });
+
           } else {
-            signerP2 = new SignerP2(keyshare, Encryptor.encodeAuthPriv(privKey), Encryptor.encodeAuthPub(pubKey))
-            console.log('(📱,ℹ️) Signer for device 2 created.')
+            console.log('(0️⃣-📱,ℹ️) Step 0 to be executed in 📱');
+            console.log('(📦,ℹ️) Payload obtained', socketPayload)
+            const parsed = JSON.parse(socketPayload);
+            const otherDevicePubKey: PubCurveBasePoint = ec.curve.point(parsed.x, parsed.y);
+            console.log('(🔑,ℹ️) PubKey parsed', pubKey, privKey, keyshare)
+            console.log('(📦,ℹ️) Message from signer 1', parsed.message1);
+            signerP2 = new SignerP2(keyshare, Encryptor.encodeAuthPriv(privKey), Encryptor.encodeAuthPub(otherDevicePubKey))
+            const message2 = await signerP2.step1(JSON.stringify(txObject), parsed.message1)
+            console.log('(📱,ℹ️) Signer for device 2 created, and signed message2.', message2)
           }
         }
       }
     }
-    counter++
-    console.log("Handler Counter", counter);
+    stepCounter++
+    console.log("Handler Counter", stepCounter);
   }
 }
