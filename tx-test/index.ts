b509f1d5b9aa1d77dd0e881260352329ec50bc9d
jjperezaguinaga <me@jjperezaguinaga.com>
2023-09-18 03:17:03 +0000

    feature: Moving to EVM RPC layer from Hedera

--- tx-test/index.ts
+++ tx-test/index.ts
@@ -1,8 +1,8 @@
 import { ethers } from 'ethers'
 
-const rawTx = "0x02f86c05018502540be4008504a817c800825208946442c72abd1a9d14c303277a8c994fae295b6bcb8080c001a0e761b3d2074283b49a499ec8af2fcf44a562848e082ba76efca1530b95e7dba2a028a6a42f7226e21c8469325b475facb30d8896cc532fa454e005a4870fea0b09"
+const rawTx = "0x02f86c05018502540be4008504a817c800825208946442c72abd1a9d14c303277a8c994fae295b6bcb8080c080a015248f0e209f101d14115adc28ab3a3fdf98bc91efa95905a313c9c94e8b6b52a04fc158f315d3422a51e3330cd926990751c9f0572c1f0a999453d248ff2293aa"
 const parsedTxData = ethers.utils.parseTransaction(rawTx)
-const provider = new ethers.providers.JsonRpcProvider("https://ethereum-goerli.publicnode.com");
+const provider = new ethers.providers.JsonRpcProvider("https://previewnet.hashio.io/api");
 
 console.log(ethers.utils.parseUnits("2", "wei"));
 console.log(ethers.utils.parseUnits("3", "wei"));
